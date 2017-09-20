191f041922002205907f0cc9121ab32febe4beac
slynch45 <slynch45@MCBA202-C12.classrooms.stlcc>
2017-09-20 11:01:57 -0500

    first commit

---
+++ src/java1githubdemo/Java1GithubDemo.java
@@ -0,0 +1,16 @@
+package java1githubdemo;
+/**
+ *
+ * @author slynch45
+ */ 
+public class Java1GithubDemo {
+    //psvm
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //sout
+        System.out.println("Hello, world!");
+    }
+    
+}
